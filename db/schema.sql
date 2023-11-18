37ea418d69a9d2d4fd66b70d5d705155490c6ce8
peta swan <petaswan986@gmail.com>
2023-11-18 12:27:13 +1100

    models code push

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,21 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- USE ecommerce_db;
+
+-- CREATE TABLE category (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     category_name VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE product (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     product_name VARCHAR(50) NOT NULL,
+--     price DECIMAL(10,2) NOT NULL,
+--     stock INT NOT NULL, 
+--     category_id INT 
+--     refe
+--     PRIMARY KEY (id)
+-- );
+
